896efefd83616cdd2d66f1af5a56dd7e63946aaa
Muhlis222 <mohmuhlis021@gmail.com>
2023-11-26 20:19:27 +0700

    tugas

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <View>
+      <Text>HELLO</Text>
+    </View>
+  )
+}
+
+export default App;
